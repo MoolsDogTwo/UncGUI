b9789a1e23fc53789fb149fb7653777d5aaa9f8f
Aleksandar Milović <107961418+MoolsDogTwo@users.noreply.github.com>
2026-04-10 12:21:07 +1000

    Fist commit

---
+++ Window.cs
@@ -0,0 +1,23 @@
+﻿namespace UncGUI;
+
+internal abstract class Window
+{
+    private readonly int _id;
+    private int _x;
+    private int _y;
+    private int _width;
+    private int _height;
+    public string Title;
+
+    public Window(int x, int y, int width, int height, string title)
+    {
+        _x = x;
+        _y = y;
+        _width = width;
+        _height = height;
+    }
+}
+
+public class BasicWindow
+{
+}
